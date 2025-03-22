383b9f38dfe27bcd2c793dc3358ad94520b30c18
Maisha Maimuna <66841427+maishapriyota@users.noreply.github.com>
2025-03-22 19:16:54 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,3 +31,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+
+  // On click, toggle .dark-mode on <body>
+  darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+});
+
